7b1013be13098a3b02fc592aa1703e49fa2defe2
rheidary42 <radinheidary2012@gmail.com>
2025-10-22 00:17:00 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -6,77 +6,100 @@
 /*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 06:37:01 by rheidary          #+#    #+#             */
-/*   Updated: 2025/10/03 06:37:02 by rheidary         ###   ########.fr       */
+/*   Updated: 2025/10/21 21:06:21 by rheidary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#define _POSIX_C_SOURCE 200809L // Docker specific
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <stdlib.h>
 
-volatile sig_atomic_t ack = 0;
+#define _POSIX_C_SOURCE 200809L
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "ft_printf/libftprintf.h"
+
+volatile sig_atomic_t	g_ack = 0;
 
 void	ack_handler(int sig)
 {
-    (void)sig;
-    ack = 1;
+	if (sig == SIGUSR1)
+		g_ack = 1;
+	if (sig == SIGUSR2)
+	{
+		write(1, "Hand has been shook\n", 21);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
 
 void	send_signal(char c, int j, char *server)
 {
-	ack = 0;
+	g_ack = 0;
 	if ((c & (1 << (7 - j))) != 0)
-		kill((pid_t)atoi(server), SIGUSR2);
+	{
+		if (kill((pid_t)ft_atoi(server), SIGUSR2) == -1)
+		{
+			ft_printf("Error\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 	else
-		kill((pid_t)atoi(server), SIGUSR1);
-	while (!ack)
+	{
+		if (kill((pid_t)ft_atoi(server), SIGUSR1) == -1)
+		{
+			ft_printf("Error\n");
+			exit (EXIT_FAILURE);
+		}
+	}
+	while (!g_ack)
 		pause();
-}
-
-int atoi(const char *str)
-{
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '+' || str[i] == '-')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return result * sign;
 }
 
 int	main(int argc, char **argv)
 {
-	int				i;
-	int				j;
-	char			c;
-	struct sigaction s_sa;
-	
-	s_sa.sa_flags = 0;
-	s_sa.sa_handler = ack_handler;
-	sigemptyset(&s_sa.sa_mask);
-	sigaction(SIGUSR1, &s_sa, NULL);
+	int					i;
+	int					j;
+	int					c;
+	struct sigaction	sa;
+
+	if (argc != 3 || ft_atoi(argv[1]) <= 0)
+		return (ft_printf("Error\n"));
+	sa.sa_flags = 0;
+	sa.sa_handler = ack_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	i = 0;
-	if (argc != 3)
-		return (1);
-	while (argv[2][i])
+	while (1)
 	{
 		c = argv[2][i];
 		j = -1;
 		while (++j < 8)
 			send_signal(c, j, argv[1]);
+		if (c == '\0')
+			break;
 		i++;
 	}
 	return (0);
--- server.c
+++ server.c
@@ -6,44 +6,112 @@
 /*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 06:37:05 by rheidary          #+#    #+#             */
-/*   Updated: 2025/10/03 06:44:32 by rheidary         ###   ########.fr       */
+/*   Updated: 2025/10/21 22:01:23 by rheidary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#define _POSIX_C_SOURCE 200809L // Docker specific
-#include <unistd.h>
-#include <stdio.h>
+
+#define _POSIX_C_SOURCE 200809L
 #include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "ft_printf/libftprintf.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin_char(char *s, char c)
+{
+	int		i;
+	char	*new;
+	if (!s)
+	{
+		new = malloc(2);
+		if (!new)
+			return (NULL);
+		new[0] = c;
+		new[1] = '\0';
+		return (new);
+	}
+	new = malloc(ft_strlen(s) + 2);
+	usleep(50);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = c;
+	new[i + 1] = '\0';
+	free(s);
+	return (new);
+}
+
+void	send_char(char c, char **s, siginfo_t *info)
+{
+	char	*temp;
+
+	temp = ft_strjoin_char(*s, c);
+	if (!temp)
+	{
+		free(*s);
+		exit(EXIT_FAILURE);
+	}
+	*s = temp;
+	if (c == '\0')
+	{
+		write(1, *s, ft_strlen(*s));
+		free(*s);
+		*s = NULL;
+		kill(info->si_pid, SIGUSR2);
+	}
+	else
+		kill(info->si_pid, SIGUSR1);
+}
 
 void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	static int	count = 0;
+	static int		count = 0;
 	static char		c = 0;
+	static char		*s = NULL;
 
+	(void)ucontext;
 	c <<= 1;
 	if (sig == SIGUSR2)
 		c |= 1;
 	count++;
 	if (count == 8)
 	{
-		write (1, &c, 1);
+		send_char(c, &s, info);
 		count = 0;
 		c = 0;
 	}
-	kill(info->si_pid, SIGUSR1);
+	else
+		kill(info->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-	struct sigaction	s_sa;
-	
-	s_sa.sa_flags = SA_SIGINFO;
-	s_sa.sa_sigaction = sig_handler;
-	sigemptyset(&s_sa.sa_mask);  
-	sigaddset(&s_sa.sa_mask, SIGUSR1);
-	sigaddset(&s_sa.sa_mask, SIGUSR2);
-	sigaction(SIGUSR1, &s_sa, NULL);
-	sigaction(SIGUSR2, &s_sa, NULL);
-	printf("Server PID: %d\n", getpid());
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 		pause();
 	return (0);
